b843ada4858de8339c2742dd7b1a19980d802161
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-07 01:54:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from langchain_helper import get_qa_chain, create_vector_db
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import GooglePalm
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
 
 st.title("Codebasics Q&A 🌱")
 btn = st.button("Create Knowledgebase")
